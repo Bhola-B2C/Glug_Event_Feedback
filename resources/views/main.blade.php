9facb1c5d92da2bc71c01f2d95acbfadb7b2aef2
Bhola-B2C <bholajrs@gmail.com>
2017-10-16 11:08:36 +0530

    Revert "Update to move custom styles into separate sass file, and update the …"

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title> GNU Linux User's Group</title>
-	<link rel="stylesheet" href="{{ asset('/css/app.css')}}">
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" href="{{ asset('/css/style.css')}}">
 	<link rel="stylesheet" href="{{ asset('/Font-Awesome/css/font-awesome.min.css')}}">
 	<link rel="shortcut icon" href="images/logo.png">
 </head>
